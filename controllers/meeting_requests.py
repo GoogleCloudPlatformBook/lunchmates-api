0c70c7921b589c688651317dd1c3bf6531be4567
Jose L <joseluis@ugiagonzalez.com>
2015-04-12 13:04:34 +0200

    · Send email to owner

--- controllers/meeting_requests.py
+++ controllers/meeting_requests.py
@@ -3,6 +3,8 @@
 import base
 from base import login_required
 
+from google.appengine.api import mail
+
 from util.format import *
 from model.model import *
 
@@ -16,7 +18,7 @@ class MeetingRequestController(base.BaseHandler):
 
         results = []
 
-        query = MeetingRequest.for_meeting(meeting_id)
+        query = MeetingRequest.for_meeting(int(meeting_id))
 
         for request in query:
             results.append(request)
@@ -27,6 +29,22 @@ class MeetingRequestController(base.BaseHandler):
     def post(self, meeting_id): # Posts a request to join a meeting
 
         # TODO Check if there's an invitation already
-        request = MeetingRequest(parent=self.user_key, meeting=ndb.Key(Meeting, meeting_id))
-        request.put()
-        self.respond(201, request)
+        meeting = Meeting.get_by_id(int(meeting_id))
+        join_request = MeetingRequest(parent=self.user_key, meeting=meeting.key)
+        join_request.put()
+
+        owner_email = meeting.owner.get().email
+        if mail.is_email_valid(owner_email):
+            self._send_join_request_notification(owner_email, self.user.nickname())
+
+        self.respond(201, join_request)
+
+
+
+    def _send_join_request_notification(self, email_address, nickname):
+
+        sender_address = 'Lunch Mate <lunchmates@appid.appspotmail.com>'
+        subject = 'Request to join your meeting'
+        body = '%s has requested to join your meeting!' % nickname
+
+        mail.send_mail(sender_address, email_address, subject, body)
